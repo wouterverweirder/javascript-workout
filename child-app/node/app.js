ae769d078851b471ac97b31e1ffd5ff8f8d3b4e4
Wouter Verweirder <wouter@aboutme.be>
2014-08-23 16:54:18 +0200

    tessel code slides

--- child-app/node/app.js
+++ child-app/node/app.js
@@ -1,14 +1,27 @@
 var Cylon = require('cylon');
-var robot = Cylon.robot({
-  connection: { name: 'arduino', adaptor: 'firmata', port: '/dev/tty.usbmodem141211' },
-  device: { name: 'servo', driver: 'servo', pin: 11 },
+Cylon.robot({
+  connections: [
+    { name: 'arduino', adaptor: 'firmata', port: '/dev/tty.usbmodem141211' },
+    { name: 'opencv', adaptor: 'opencv' }
+  ],
+  devices: [
+    { name: 'servo', driver: 'servo', pin: 11, connection: "arduino" },
+    { name: 'camera', driver: 'camera', camera: 0, connection: "opencv", haarcascade: __dirname + "/haarcascade_frontalface_alt.xml" }
+  ],
   work: function(my) {
-  	var isMax = false;
-    setInterval(function(){
-      if(isMax) my.servo.angle(180);
-      else my.servo.angle(0);
-      isMax = !isMax;
-    }, 1000);
+    var angle = 0;
+    my.camera.once('cameraReady', function() {
+      my.camera.on('facesDetected', function(err, im, faces) {
+        if(faces.length > 0) {
+          angle = (faces[0].x).fromScale(1024, 0).toScale(0, 180);
+          my.servo.angle(angle);
+        }
+        my.camera.readFrame();
+      });
+      my.camera.on('frameReady', function(err, im) {
+        my.camera.detectFaces(im);
+      });
+      my.camera.readFrame();
+    });
   }
-});
-robot.start();
+}).start();
