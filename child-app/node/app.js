48d36fedc493c74cc21dd1ea08696cc1f0fc8b5b
Wouter Verweirder <wouter.verweirder@gmail.com>
2016-10-30 12:56:27 +0100

    file mode changes

--- child-app/node/app.js
+++ child-app/node/app.js
@@ -1,18 +1,38 @@
 var Cylon = require('cylon');
 var robot = Cylon.robot({
-	connections: {
-		arduino: {
-			adaptor: 'firmata',
-			port: '/dev/tty.usbmodem14111',
-			devices: {
-				led:    { driver: 'led', pin: 13 },
-			}
-		}
-	},
-	work: function(my) {
-    setInterval(function(){
-      my.led.toggle();
-    }, 250);
+  connections: {
+    arduino: {
+      adaptor: 'firmata', port: '/dev/tty.usbmodem14111',
+      devices: {
+        servo:    { driver: 'servo', pin: 11 },
+      }
+    },
+    opencv: {
+      adaptor: 'opencv',
+      devices: {
+        camera: {
+          driver: 'camera',
+          camera: 0,
+          haarcascade: __dirname + "/haarcascade_frontalface_alt.xml"
+        }
+      }
+    }
+  },
+  work: function(my) {
+    var angle = 0;
+    my.camera.once('cameraReady', function() {
+      my.camera.on('facesDetected', function(err, im, faces) {
+        if(faces.length > 0) {
+          console.log(faces.length, faces[0].x);
+          angle = (faces[0].x).fromScale(1024, 0).toScale(0, 180);
+          my.servo.angle(angle);
+        }
+        my.camera.readFrame();
+      });
+      my.camera.on('frameReady', function(err, im) {
+        my.camera.detectFaces(im);
+      });
+      my.camera.readFrame();
+    });
   }
-});
-robot.start();
+}).start();
